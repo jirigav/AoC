9522fa2302c0a1a8b2f015a43ebe2ce1a739d85e
jirigav <jirigavenda98@gmail.com>
2021-12-11 10:58:13 +0100

    day11

---
+++ 2021-Rust/day11/src/main.rs
@@ -0,0 +1,101 @@
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn read_lines_to_map(file_path: &str) -> HashMap<(usize, usize), u32> {
+    let file = File::open(file_path).expect("file wasn't found.");
+    let reader = BufReader::new(file);
+
+    let mut map: HashMap<(usize, usize), u32> = HashMap::new();
+    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+
+    for i in 0..lines.len() {
+        let chars: Vec<char> = lines[i].chars().collect();
+        for j in 0..chars.len() {
+            map.insert((i, j), chars[j].to_digit(10).unwrap());
+        }
+    }
+    map
+}
+
+fn get_neighbours(i: usize, j: usize) -> Vec<(usize, usize)> {
+    let mut neighbours: Vec<(usize, usize)> = Vec::new();
+    for a in [-1, 0, 1] {
+        for b in [-1, 0, 1] {
+            if a == 0 && b == 0 {
+                continue;
+            }
+            if ((i as i32) + a) >= 0
+                && ((i as i32) + a) < 10
+                && ((j as i32) + b) >= 0
+                && ((j as i32) + b) < 10
+            {
+                neighbours.push((((i as i32) + a) as usize, ((j as i32) + b) as usize))
+            }
+        }
+    }
+    neighbours
+}
+
+fn increment_all(map: &mut HashMap<(usize, usize), u32>) -> bool {
+    let mut found = false;
+    for i in 0..10 {
+        for j in 0..10 {
+            let new_val = map.get(&(i, j)).unwrap() + 1;
+            map.insert((i, j), new_val);
+
+            if new_val > 9 {
+                found = true;
+            }
+        }
+    }
+    found
+}
+
+fn part1(mut map: HashMap<(usize, usize), u32>, is_part2: bool) -> usize {
+    let mut counter = 0;
+    let mut r = 1;
+    loop {
+        let mut flashed: HashSet<(usize, usize)> = HashSet::new();
+        let mut found = increment_all(&mut map);
+
+        while found {
+            found = false;
+
+            for i in 0..10 {
+                for j in 0..10 {
+                    let val = *map.get(&(i, j)).unwrap();
+                    if val > 9 {
+                        found = true;
+                        map.insert((i, j), 0);
+                        flashed.insert((i, j));
+
+                        for (a, b) in get_neighbours(i, j).into_iter() {
+                            if map.contains_key(&(a, b)) && !flashed.contains(&(a, b)) {
+                                map.insert((a, b), map.get(&(a, b)).unwrap() + 1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        counter += flashed.len();
+        if r == 100 && !is_part2 {
+            break;
+        }
+
+        if flashed.len() == 100 {
+            return r;
+        }
+        r += 1
+    }
+    counter
+}
+
+fn main() {
+    let map = read_lines_to_map("./input");
+
+    println!("{:?}", part1(map.clone(), false));
+    println!("{:?}", part1(map, true));
+}
